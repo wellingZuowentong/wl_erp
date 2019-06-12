a286a581675af8c991a34fb958cdcad30dfb8b76
weilingtianrui <2427076316@qq.com>
2019-06-12 18:25:56 +0800

    1

--- src/main/java/com/weiling/wl_erp/controller/ShangPinController.java
+++ src/main/java/com/weiling/wl_erp/controller/ShangPinController.java
@@ -5,8 +5,10 @@ import com.weiling.wl_erp.service.ShangPinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 /**

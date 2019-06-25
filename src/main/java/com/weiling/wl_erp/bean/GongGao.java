30d8b045436c206a33828781aa6725b8fb0999ce
wanghuaipeng666 <1055665333@qq.com>
2019-06-25 08:36:51 +0800

    欢迎页加入2

--- src/main/java/com/weiling/wl_erp/bean/GongGao.java
+++ src/main/java/com/weiling/wl_erp/bean/GongGao.java
@@ -1,8 +1,64 @@
 package com.weiling.wl_erp.bean;
 
+import java.util.Date;
+
 /**
  * 作者：王怀朋
  * 日期：2019/6/25
  */
 public class GongGao {
+    private Integer id;
+    private String biaoti;
+    private String neirong;
+    private Date uptime;
+    public GongGao(){}
+
+    public GongGao(Integer id, String biaoti, String neirong, Date uptime) {
+        this.id = id;
+        this.biaoti = biaoti;
+        this.neirong = neirong;
+        this.uptime = uptime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBiaoti() {
+        return biaoti;
+    }
+
+    public void setBiaoti(String biaoti) {
+        this.biaoti = biaoti;
+    }
+
+    public String getNeirong() {
+        return neirong;
+    }
+
+    public void setNeirong(String neirong) {
+        this.neirong = neirong;
+    }
+
+    public Date getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(Date uptime) {
+        this.uptime = uptime;
+    }
+
+    @Override
+    public String toString() {
+        return "GongGao{" +
+                "id=" + id +
+                ", biaoti='" + biaoti + '\'' +
+                ", neirong='" + neirong + '\'' +
+                ", uptime=" + uptime +
+                '}';
+    }
 }
--- src/main/java/com/weiling/wl_erp/mapper/GongGaoMapper.java
+++ src/main/java/com/weiling/wl_erp/mapper/GongGaoMapper.java
@@ -1,8 +1,19 @@
 package com.weiling.wl_erp.mapper;
 
+import com.weiling.wl_erp.bean.GongGao;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * 作者：王怀朋
  * 日期：2019/6/25
  */
+@Mapper
 public interface GongGaoMapper {
+    public int insertGG(GongGao gongGao);
+    public List<GongGao> findAllGongGao();
+    public GongGao findGongGaoById(Integer id);
+    public int delGongGaoById(Integer id);
 }
